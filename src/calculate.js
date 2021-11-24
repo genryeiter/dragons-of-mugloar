49aabfa334293ad8822963fea1ff6f2133f70006
gera <german@parsiq.net>
2021-11-24 11:50:43 +0200

    adding test for further automations

---
+++ src/calculate.js
@@ -0,0 +1,3 @@
+const Calculate = (number) => number
+
+module.exports = Calculate
---
+++ src/calculate.test.js
@@ -0,0 +1,6 @@
+const Calculate = require("./calculate");
+
+test('should calc properly', () => {
+    const calc = Calculate(5)
+    expect(calc).toEqual(5)
+})
